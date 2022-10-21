9f2ae8d210d61ccc6486632a24cd3dee8cc66124
Daly Barron <dalygbarron@gmail.com>
2022-10-21 09:21:10 +0100

    added some nice colours

--- curses/main.cc
+++ curses/main.cc
@@ -3,6 +3,11 @@
 #include <string.h>
 #include "Map.h"
 
+#define COLOUR 1
+
+/**
+ * Does the needful.
+ */
 void coolStuff() {
 	Map map(Vector(COLS, LINES));
 	for (int x = 0; x < map.size.x; x++) {
@@ -10,21 +15,29 @@ void coolStuff() {
 			map.setTile(Vector(x, y), 0, 32 + rand() % 94);
 		}
 	}
+	attron(COLOR_PAIR(COLOUR) | A_UNDERLINE | A_BOLD);
 	for (int y = 0; y < map.size.y; y++) {
 		move(y, 0);
 		for (int x = 0; x < map.size.x; x++) {
 			addch(map.getTile(Vector(x, y), 0));
 		}
 	}
+	attroff(COLOR_PAIR(COLOUR) | A_UNDERLINE | A_BOLD);
 	getch();
 }
 
-int main() {
+/**
+ * Entrypoint of da program.
+ * @param argc is the number of arguments.
+ * @param argv is the value of the arguments.
+ */
+int main(int argc, char **args) {
 	initscr();
 	start_color();
 	cbreak();
 	noecho();
 	keypad(stdscr, TRUE);
+	init_pair(COLOUR, COLOR_GREEN, COLOR_RED);
 	coolStuff();
 	endwin();
 }
